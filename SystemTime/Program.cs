001cf2b9d4e3e6bd420722cfd9273328bcd8a90a
DatNguyen2002 <90488040+DatNguyen2002@users.noreply.github.com>
2023-04-27 19:53:57 -0700

    print the current date and time done

---
+++ SystemTime/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SystemTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime localDate = DateTime.Now;
+            System.Console.WriteLine("Date and Time now is:" + localDate);
+        }
+    }
+}
